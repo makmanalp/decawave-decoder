8d7b8f923907e5c3f1d689f7cc5e3648d301007d
I am the borg <kratos@robot.boop>
2015-05-25 21:14:08 -0400

    Felipe Hack to reset decawave

--- kratos_decawave.sh
+++ kratos_decawave.sh
@@ -1,1 +1,13 @@
+# Port setting
+stty -F /dev/kratos_decawave raw speed 115200
+
+echo '?' > /dev/kratos_decawave
+echo '?' > /dev/kratos_decawave
+
+echo '#' > /dev/kratos_decawave
+echo '#' > /dev/kratos_decawave
+echo '#' > /dev/kratos_decawave
+echo '#' > /dev/kratos_decawave
+
+sleep 2
 stdbuf -oL -eL ./spisniffer -d /dev/kratos_decawave | python -u lcd_spi_decode.py
